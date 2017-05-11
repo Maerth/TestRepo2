074071a08a4e5c159a7c45123e7aa4d65826c92f
Martijn Spieker <classysmashed@hotmail.com>
2017-05-11 09:47:35 +0200

    Logd change

--- app/src/main/java/com/maerth/xyz/gittest2/MainActivity.java
+++ app/src/main/java/com/maerth/xyz/gittest2/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("String", "onCreate: ");
+        Log.d("String", "Koen is een kneus");
     }
 }
